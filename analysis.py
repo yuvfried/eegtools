d75baa6ed6d567719d7d964edf2bc4a438e804cb
Yuval <yuvalf@elminda.com>
2020-08-14 11:51:14 +0300

    auc

--- analysis.py
+++ analysis.py
@@ -2,7 +2,6 @@ import numpy as np
 
 
 class Component:
-    # TODO: AUC
     # TODO: account for timeline boundaries corresponding to N1, P2 etc
     def __init__(self, arr, orientation, baseline=0):
         self.sig = arr
@@ -13,9 +12,15 @@ class Component:
         return np.sqrt(np.nanmean((self.sig-self.baseline)**2, **kwargs))
 
     def peak(self, **kwargs):
+        dist = np.abs(self.sig - self.baseline)
         if self.orientation == "N":
-            return np.min(self.sig, **kwargs)
+            return np.min(dist, **kwargs)
         if self.orientation == "P":
-            return np.max(self.sig, **kwargs)
+            return np.max(dist, **kwargs)
+
+    def auc(self, **kwargs):
+        # TODO: account for timeline copy from notebook
+        return np.trapz(y=np.abs(self.sig-self.baseline))
+
 
 
