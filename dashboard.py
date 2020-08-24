3c5a774363d9a255c9ac91d8b69765e068a70fc7
Yuval <yuvalf@elminda.com>
2020-08-24 19:47:39 +0300

    granular subject dash

--- dashboard.py
+++ dashboard.py
@@ -1,7 +1,8 @@
 import plot
+import numpy as np
 
 
-# TODO: append data object to this module
+
 def group_erp_block_trial_range(fig, group_name, block_range, trial_range, timeline, cmap):
     """
 Group ERP Dynamics over a given Range of Blocks and Trials
@@ -27,6 +28,54 @@ Group ERP Dynamics over a given Range of Blocks and Trials
                          name=
                          f'{group_name}, {block_range[0] - block_range[1]}, {trial_range[0]}-{trial_range[1]}',
                          line_color=cmap[group_name])
+    fig.add_trace(gos)
+
+    fig.show()
+
+
+# averaging chronological order: trial-->block-->subject
+def subject_erp_block_trial_range(fig, data, sub_id, block, trial, timeline, cmap,
+                                   block_end=False, trial_end=False, show_err="blocks"):
+    idx = np.argwhere(data['subjects'] == sub_id).flatten()[0]
+    group = data['group'][idx]
+    block -= 1  # adjust python index
+    trial -= 1  # adjust python index
+
+    # multiple blocks and trials
+    if not block_end:
+        block_end = block+1
+    else:
+        block_end -= 1
+    if not trial_end:
+        trial_end = trial+1
+    else:
+        trial_end -= 1
+
+    # gather data
+    sig = data['s2'][idx, :, trial:trial_end, block:block_end]
+
+    # error bars
+    if show_err=="blocks":
+        err = np.nanstd(sig, axis=2)
+    elif show_err=="trials":
+        err = np.nanstd(sig, axis=1)
+    else:
+        err = np.zeros(512)
+
+    # averaging
+    if sig.shape[1] > 1:   # multiple trials
+        sig = np.nanmean(sig, axis=1)  # averaging over trials
+    if sig.shape[1] > 1:   # multiple blocks
+        sig = np.nanmean(sig, axis=1)  # averaging over blocks
+
+    sig = sig.reshape(512,)
+
+    gos = plot.go_signal(sig, timeline,
+                         error_y=dict(
+                             type="data", array=err / 2, visible=True, color=cmap[group]["error"]),
+                         name=f'{sub_id} {block} {trial}',
+                         line_color=plot.cmap[group]["line"],
+                         showlegend=True)
     fig.add_trace(gos)
 
     fig.show()
