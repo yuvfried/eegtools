d2e14f996f3c5bec3d3293e8d62e6ad5234ba301
Yuval <yuvalf@elminda.com>
2020-08-25 12:05:02 +0300

    ERPSignal

--- analysis.py
+++ analysis.py
@@ -1,19 +1,19 @@
 import numpy as np
 from data_ingestion import mat_data
 
-
 TIMELINE = mat_data['time']
 
 
-def adjust_python_idx(idx):
-    return idx-1
+def _adjust_python_idx(idx):
+    return idx - 1
 
 
 class Signal:
 
-    def __init__(self, values, timeline=TIMELINE):
+    def __init__(self, values, timeline=TIMELINE, noise=None):
         self.values = values
-        self.baseline = timeline
+        self.timeline = timeline
+        self.noise = noise
 
 
 class EEGSignal(Signal):
@@ -21,8 +21,8 @@ class EEGSignal(Signal):
     def __init__(self, name, trial, block, timeline=TIMELINE, data=mat_data):
         self.name = name
         self.sub_id = np.argwhere(data['subjects'] == name).flatten()[0]
-        self.trial = adjust_python_idx(trial)
-        self.block = adjust_python_idx(block)
+        self.trial = _adjust_python_idx(trial)
+        self.block = _adjust_python_idx(block)
         self.values = data['s2'][self.sub_id, :, self.trial, self.block]
         super().__init__(self.values, timeline)
 
@@ -32,58 +32,65 @@ class ERPSignal:
     def __init__(self, name, trials, blocks, timeline=TIMELINE, data=mat_data):
         self.name = name
         self.sub_id = np.argwhere(data['subjects'] == name).flatten()[0]
-        self.data = data
         self.timeline = timeline
-        self.avg_over = self.__discover_avg_over(trials, blocks)
-        self.__set_values()
+        # init trial-block range
+        self.trial, self.block, self.trial_start, self.block_start, \
+            self.trial_end, self.block_end, self.avg_over = tuple([None] * 7)
+        self.data = self.__auto_set_data(trials, blocks, data)
         self.__signal = None
         self.__noise = None
         # self.desc = f"ERPSignal averaged over {self.avg_over}"
 
-    def set_avg(self, over):
-        self.avg_over = over
-        self.__set_values()
+    def __unpack_range(self, pack, axis):
+        if axis == "trials":
+            if isinstance(pack, int):
+                self.trial = _adjust_python_idx(pack)
+            else:
+                self.trial_start = _adjust_python_idx(pack[0])
+                self.trial_end = _adjust_python_idx(pack[1])
+        if axis == "blocks":
+            if isinstance(pack, int):
+                self.block = _adjust_python_idx(pack)
+            else:
+                self.block_start = _adjust_python_idx(pack[0])
+                self.block_end = _adjust_python_idx(pack[1])
 
-    def __discover_avg_over(self, trials, blocks):
-        if isinstance(self.blocks, int):
-            if isinstance(self.trials, int):
+    def __auto_set_data(self, trials, blocks, data):
+        self.__unpack_range(trials, "trials")
+        self.__unpack_range(blocks, "blocks")
+        if self.block is not None:
+            if self.trial is not None:
                 raise TypeError("A stochastic combination of trials and blocks"
                                 "was passed. Please use EEGSignal(*args).")
             else:
-                return "trial"
+                self.avg_over = "trial"
+                return data['s2'][self.sub_id, :,
+                       self.trial_start:self.trial_end+1, self.block]
         else:
-            if isinstance(self.trials, int):
-                return "block"
+            if self.trial is not None:
+                self.avg_over = "block"
+                return data['s2'][self.sub_id, :,
+                       self.trial, self.block_start:self.block_end+1]
             else:
-                return None
-
-    def __set_values(self):
-        if self.avg_over == "trial":
-            self.block = adjust_python_idx(blocks)
-            self.trial_start, self.trial_end = adjust_python_idx(self.trials[0]), adjust_python_idx(self.trials[1])
-            self.values = self.data['s2'][sub_id,:, self.trial_start:self.trial_end,self.block]
-        if self.avg_over == "block":
-            self.block_start, self.block_end = adjust_python_idx(self.blocks[0]), adjust_python_idx(self.blocks[1])
-            self.trial = adjust_python_idx(trials)
-            self.values = self.data['s2'][sub_id,:, self.trial,self.block_start:self.block_end]
-        if self.avg_over is None:
-            self.trial_start, self.trial_end = adjust_python_idx(self.trials[0]), adjust_python_idx(self.trials[1])
-            self.block_start, self.block_end = adjust_python_idx(self.blocks[0]), adjust_python_idx(self.blocks[1])
-            self.values = self.data['s2'][sub_id,:, self.trial_start:self.trial_end, self.block_start:self.block_end]
+                return data['s2'][self.sub_id, :,
+                       self.trial_start:self.trial_end+1,
+                       self.block_start:self.block_end+1]
 
     def fit(self):
         if self.avg_over is None:
-            self.__signal = np.nanmean(self.values, axis=(1,2))
-            self.__noise = np.nanstd(self.values, axis=(1,2))
-        else:   # trial or block are both will be on axis=1
-            self.__signal = np.nanmean(self.values, axis=1)
-            self.__noise = np.std(self.values, axis=1)
+            self.__signal = np.nanmean(self.data, axis=(1, 2))
+            self.__noise = np.nanstd(self.data, axis=(1, 2))
+        else:  # trial or block are both will be on axis=1
+            self.__signal = np.nanmean(self.data, axis=1)
+            self.__noise = np.std(self.data, axis=1)
 
-    def signal(self):
-        return Signal(self.__signal, self.timeline)
+        return Signal(self.__signal, self.timeline, self.__noise)
 
-    def noise(self):
-        return self.__noise
+    # def signal(self):
+    #     if self.__signal is None:
+    #         raise Exception("Use fit method in order to output ERP signal")
+    #     else:
+    #         return Signal(self.__signal, self.timeline, self.__noise)
 
 
 class Component:
@@ -95,21 +102,21 @@ class Component:
         self.values = signal.values[boundaries[0]:boundaries[1]]
         self.baseline = baseline
 
-    def __str__(self):
+    def __repr__(self):
         return f'Component {self.orientation}{self.t1}-{self.t2} post-onset'
 
     def __get_t_idx(self, timeline, t):
         vf = np.vectorize(lambda t: np.argmin(np.abs(timeline - t)))
         return vf(t)  # idx for corresponding t (same length)
 
-    def summation(self, **kwargs):
-        return np.nansum(self.values-self.baseline, **kwargs)
+    def sum(self, **kwargs):
+        return np.nansum(self.values - self.baseline, **kwargs)
 
-    def abs_summation(self):
-        return np.nansum(np.abs(self.values-self.baseline))
+    def abs_sum(self):
+        return np.nansum(np.abs(self.values - self.baseline))
 
     def rms(self, **kwargs):
-        return np.sqrt(np.nanmean(self.values-self.baseline, **kwargs))
+        return np.sqrt(np.nanmean(self.values - self.baseline, **kwargs))
 
     def peak(self, **kwargs):
         if self.orientation == "N":
@@ -119,19 +126,18 @@ class Component:
 
     def auc(self, absolute_val=False, **kwargs):
         if absolute_val:
-            return np.trapz(y=np.abs(self.values-self.baseline), x=self.timeline)
+            y = np.abs(self.values - self.baseline)
         else:
-            return np.trapz(y=self.values-self.baseline, x=self.timeline)
+            y = self.values - self.baseline
+        return np.trapz(y=y, x=self.timeline, **kwargs)
 
 
 def softmax(x):
-    return np.exp(x)/np.nansum(np.exp(x))
+    return np.exp(x) / np.nansum(np.exp(x))
 
 
 # note assumes positive vector (consider N1)?
 def zero_one_scaling(x):
     mini = np.nanmin(x)
     maxi = np.nanmax(x)
-    return (x-mini)/(maxi-mini)
-
-
+    return (x - mini) / (maxi - mini)
